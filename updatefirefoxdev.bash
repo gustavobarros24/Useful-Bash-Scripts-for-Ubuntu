9102b9522e32eacc4e1675332b3e23749b7e4326
Gustavo Barros <93828237+gustavobarros24@users.noreply.github.com>
2025-02-02 16:09:26 +0000

    O firefox trocou o tipo do arquivo por isso tive que dar update a isto, que chatos, mas pelo menos ja funciona

--- updatefirefoxdev.bash
+++ updatefirefoxdev.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Execute the first command wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O Firefox-dev.tar.bz2
-wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O Firefox-dev.tar.bz2
+# Execute the first command
+wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O Firefox-dev.tar.xz
 # Wait for command1 to finish, then execute the second comman with sudo
-sudo tar xjf  Firefox-dev.tar.bz2 -C /opt
+sudo tar xJf Firefox-dev.tar.xz -C /opt
